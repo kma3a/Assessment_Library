8f980feda0b742457041285966f50a69cdf0aaa5
Kelsey Ando <kma3a@virginia.edu>
2015-03-20 09:27:52 -0400

    adding constructor object for the books

---
+++ library.js
@@ -0,0 +1,7 @@
+function Book(title, author, genre, length, isCheckedIn){
+	this.title = title;
+	this.author = author;
+	this.genre = genre;
+	this.length = length;
+	this.isCheckedIn = isCheckedIn;
+}
