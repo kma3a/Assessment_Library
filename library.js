e04da7376ba3a2df41bd16a78e66091374ba1813
Kelsey Ando <kma3a@virginia.edu>
2015-03-20 09:43:12 -0400

    adding some tests to make sure that my add book method on library works

--- library.js
+++ library.js
@@ -26,3 +26,10 @@ Library.prototype.removeBook = function(book) {
 	var bookIndex = this.bookInventory.indexOf(book);
 	this.bookInventory.splice(bookIndex,1);
 }
+
+var myLibrary = new Library;
+var hello = new Book("Hitchhiker's Guide to The Galaxy", "Douglas Adams", "Science Fiction", 42);
+var anotherBook = new Book("Ella Enchanted", "Some Author", "Fantasy", 30);
+myLibrary.addBook(hello);
+myLibrary.addBook(anotherBook);
+console.log(myLibrary.bookInventory.length === 2);
