8c5973b56d3d571896352256a6b3efa94c0a2a35
Kelsey Ando <kma3a@virginia.edu>
2015-03-20 09:32:32 -0400

    adding method to books to change the checked in and out status and changed isCheckedIn to be a boolean that the librarian does not change on their own

--- library.js
+++ library.js
@@ -1,7 +1,15 @@
-function Book(title, author, genre, length, isCheckedIn){
+function Book(title, author, genre, length){
 	this.title = title;
 	this.author = author;
 	this.genre = genre;
 	this.length = length;
-	this.isCheckedIn = isCheckedIn;
+	this.isCheckedIn = true;
+}
+
+Book.prototype.changeCheckedIn = function() {
+	if( this.isCheckedIn === true) {
+		this.isCheckedIn = false;
+	} else {
+		this.isCheckedIn = true;
+	}
 }
